2dfebee4a6905868988ed2ca851acb5280558fbd
Tariq Ahmad <your_tariq.agnie01@gmail.com>
2026-07-11 13:23:42 +0530

    Restore LatestNews component

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,12 @@ import Hero from "@/components/Hero";
 import Impact from "@/components/Impact";
 import FeatureCards from "@/components/FeatureCards";
 import Mission from "@/components/Mission";
+import WaleedStory from "@/components/WaleedStory";
 import FeaturedResources from "@/components/FeaturedResources";
 import About from "@/components/About";
 import Education from "@/components/Education";
 import MPSSection from "@/components/MPSSection";
 import Footer from "@/components/Footer";
-import WaleedStory from "@/components/WaleedStory";
-import LatestNews from "@/components/LatestNews";
-
 
 export default function Home() {
   return (
@@ -20,16 +18,11 @@ export default function Home() {
       <Impact />
       <FeatureCards />
       <Mission />
-      <Mission />
       <WaleedStory />
-      <FeaturedResources />
       <FeaturedResources />
       <About />
       <Education />
       <MPSSection />
-      <Education />
-      <MPSSection />
-      <LatestNews />
       <Footer />
     </>
   );
